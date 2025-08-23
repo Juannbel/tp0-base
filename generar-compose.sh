653bb91ea862d9718eb7872f468c7c738e5f7c54
Juan Bel <juanbel1905@gmail.com>
2025-08-22 23:34:48 -0300

    Generación dinamica de docker compose con bash scripting

---
+++ generar-compose.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Uso: $0 <nombre_archivo_salida> <cantidad_clientes>"
+    exit 1
+fi
+
+echo "name: tp0" > $1
+
+echo "
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net" >> $1
+
+for i in $(seq 1 $2); do
+    echo "
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server" >> $1
+done
+
+echo "
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24" >> $1
